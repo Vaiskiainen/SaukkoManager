4a4ce8cde875a98323a8a7bac2309c6980190dfe
Vaiskiainen <164906395+Vaiskiainen@users.noreply.github.com>
2025-11-05 14:32:27 +0000

    updated download url

--- app/src/main/java/app/revenge/manager/domain/manager/DownloadManager.kt
+++ app/src/main/java/app/revenge/manager/domain/manager/DownloadManager.kt
@@ -24,7 +24,7 @@ class DownloadManager(
 
     suspend fun downloadMod(out: File, onProgressUpdate: (Float?) -> Unit) =
         download(
-            "https://github.com/revenge-mod/revenge-xposed/releases/latest/download/app-release.apk",
+            "https://github.com/Vaiskiainen/SaukkoXposed/releases/latest/download/app-release.apk",
             out,
             onProgressUpdate
         )
